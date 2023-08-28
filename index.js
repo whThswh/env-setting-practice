5216f8d676e1f495c8f015ba326518dbce0e6c56
JungHyun <wjstkwhwjdgus@gmail.com>
2023-08-28 18:06:40 +0900

    eslint & prettier setting

---
+++ index.js
@@ -0,0 +1,16 @@
+// // prettier setting practice
+// var foo = x => {
+//   console.log('foo');
+// };
+
+// ('asdffdf');
+
+// ('12313141234');
+
+// // 1. node_modules/prettier
+// // 2. prettier run(terminal)
+
+// eslint setting practice
+const foo = 'Hello World';
+
+console.info(foo);
